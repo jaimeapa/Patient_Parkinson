b91cbf8bc0a10a5c1601fa495c61d8e13607424f
jaimeapa <jaime.apariciohalpern@usp.ceu.es>
2024-11-02 12:17:45 +0100

    first commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.patient_parkinson {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.patient_parkinson to javafx.fxml;
-    exports com.example.patient_parkinson;
-}
