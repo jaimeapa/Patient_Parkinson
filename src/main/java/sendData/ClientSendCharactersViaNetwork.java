e734e232153e9461f8a8070db431399caa5c2f24
pablorosalesrodriguez <pablo.rosalesrodriguez@usp.ceu.es>
2024-11-06 17:55:49 +0100

    client send characters and strings

---
+++ src/main/java/sendData/ClientSendCharactersViaNetwork.java
@@ -0,0 +1,52 @@
+package sendData;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ClientSendCharactersViaNetwork {
+
+    public static void main(String args[]) throws IOException {
+        int byteRead;
+        //Redirect the console
+        InputStream console = (System.in);
+        //You should change localhost by the IP address 
+        //We are connecting to a "service" in an IP and port 9000
+        Socket socket = new Socket("localhost", 9000);
+        OutputStream outputStream = socket.getOutputStream();
+
+        while (true) {
+            //Read from our console
+            byteRead = console.read();
+            //And send it to the server
+            outputStream.write(byteRead);
+            if (byteRead == -1 || byteRead == 'x') {
+                System.out.println("Finish");
+                releaseResources(outputStream, console, socket);
+                System.exit(0);
+            }
+        }
+    }
+
+    private static void releaseResources(OutputStream outputStream,
+            InputStream console, Socket socket) {
+        try {
+            try {
+                console.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ClientSendCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                outputStream.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ClientSendCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientSendCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/main/java/sendData/SendStringsViaNetwork.java
@@ -0,0 +1,36 @@
+package sendData;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SendStringsViaNetwork {
+
+    public static void main(String args[]) throws IOException {
+        System.out.println("Starting Client...");
+        Socket socket = new Socket("localhost", 9009);
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+        System.out.println("Connection established... sending text");
+        printWriter.println("Header File\n\n");
+        printWriter.println("Tell me, what is it you plan");
+        printWriter.println("to do with your one wild");
+        printWriter.println("and precious life?");
+        printWriter.println("Mary Oliver");
+        System.out.println("Sending stop command");
+        printWriter.println("stop");
+        releaseResources(printWriter, socket);
+        System.exit(0);
+    }
+
+    private static void releaseResources(PrintWriter printWriter, Socket socket) {
+        printWriter.close();
+ 
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SendStringsViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
