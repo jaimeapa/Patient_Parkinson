7a379087e4f9e738c46deaf6d36dc2a9a48d3827
phita <pablo.hitalara@usp.ceu.es>
2024-11-09 12:38:37 +0100

    Signal EDA y EMG

--- src/main/java/Pojos/Signal.java
+++ src/main/java/Pojos/Signal.java
@@ -12,15 +12,25 @@ import java.util.logging.Logger;
 
 public class Signal {
     private List<Integer> valuesEMG;
+    private List<Integer> valuesEDA;
     private String patientName;
     private LocalDate beginDate;
     private String EMGFilename;
+    private String EDAFilename;
+
+
 
     public Signal(List<Integer> valuesEMG, String patientName, LocalDate beginDate, String EMGFilename) {
         this.valuesEMG = valuesEMG;
         this.patientName = patientName;
         this.beginDate = beginDate;
         this.EMGFilename = EMGFilename;
+    }
+    public Signal(List<Integer> valuesEDA, String patientName, LocalDate beginDate, String EDAFilename) {
+        this.valuesEDA = valuesEDA;
+        this.patientName = patientName;
+        this.beginDate = beginDate;
+        this.EDAFilename = EDAFilename;
     }
     public Signal(List<Integer> valuesEMG, String patientName) {
         this.valuesEMG = valuesEMG;
@@ -58,6 +68,21 @@ public class Signal {
     public void setEMGFilename(String EMGFilename) {
         this.EMGFilename = EMGFilename;
     }
+    public List<Integer> getValuesEDA() {
+        return valuesEDA;
+    }
+
+    public void setValuesEDA(List<Integer> valuesEDA) {
+        this.valuesEDA = valuesEDA;
+    }
+
+    public String getEDAFilename() {
+        return EDAFilename;
+    }
+
+    public void setEDAFilename(String EDAFilename) {
+        this.EDAFilename = EDAFilename;
+    }
 
     public void StoreEMGinFile(){
 
@@ -66,7 +91,7 @@ public class Signal {
         try {
             CreateEMGFilename();
             String ruta = "MeasurementsBitalino\\" + EMGFilename +".txt";
-            String contenido = getSignalValues(10);//this.EMG_values.toString();
+            String contenido = getSignalValuesEMG(10);//this.EMG_values.toString();
 
             File file = new File(ruta);
             if (!file.exists()) {
@@ -101,7 +126,7 @@ public class Signal {
         this.EMGFilename = patientName+"EMG"+day+month+year+"_"+hour+minute+second+millisecond+".txt";
     }
 
-    public String getSignalValues(int samplingRate) {
+    public String getSignalValuesEMG(int samplingRate) {
 
         String result = "";
         for (int j=0; j<samplingRate; j++) {
@@ -112,6 +137,63 @@ public class Signal {
             for (int i=0; i<blockSize; i++) {
                 int value = j * blockSize + i;
                 result = result + ("\n Value " + (value+1) + ": " + valuesEMG.get(value));
+            }
+        }
+        return result;
+    }
+    public void StoreEDAinFile(){
+
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+            CreateEMGFilename();
+            String ruta = "MeasurementsBitalino\\" + EDAFilename +".txt";
+            String contenido = getSignalValuesEDA(10);
+
+            File file = new File(ruta);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+            bw.write(contenido);
+
+        } catch (IOException ex) {
+            Logger.getLogger(Signal.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                bw.close();
+                fw.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Signal.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void CreateEDAFilename (){
+
+        Calendar c = Calendar.getInstance();
+        String day = Integer.toString(c.get(Calendar.DATE));
+        String month = Integer.toString(c.get(Calendar.MONTH));
+        String year = Integer.toString(c.get(Calendar.YEAR));
+        String hour = Integer.toString(c.get(Calendar.HOUR));
+        String minute = Integer.toString(c.get(Calendar.MINUTE));
+        String second = Integer.toString(c.get(Calendar.SECOND));
+        String millisecond = Integer.toString(c.get(Calendar.MILLISECOND));
+        this.EDAFilename = patientName+"EDA"+day+month+year+"_"+hour+minute+second+millisecond+".txt";
+    }
+
+    public String getSignalValuesEDA(int samplingRate) {
+
+        String result = "";
+        for (int j=0; j<samplingRate; j++) {
+            // each time reads a block of 10 samples
+            int blockSize = samplingRate;
+            //Frame[] frames = bitalino.read(blockSize);
+            result = result + "\nSize blocks: " + (j+1) + "/" + blockSize;
+            for (int i=0; i<blockSize; i++) {
+                int value = j * blockSize + i;
+                result = result + ("\n Value " + (value+1) + ": " + valuesEDA.get(value));
             }
         }
         return result;
